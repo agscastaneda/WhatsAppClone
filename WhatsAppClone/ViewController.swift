019f49ebab532c4667ad7662b83e70d738d30eeb
Agustin Castañeda <ags.castaneda@gmail.com>
2020-02-26 21:55:46 -0600

    Initial Commit

---
+++ WhatsAppClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsAppClone
+//
+//  Created by Agustin Castaneda on 26/02/20.
+//  Copyright © 2020 Agustin Castaneda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
